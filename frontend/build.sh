783701282da760bc2d6fa0a84328db730beebd2d
Vasyl Herman 🌥 <vasilgerman@gmail.com>
2026-03-26 09:18:48 +0200

    fix(mx-444): add build sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 echo "Running build tasks..."
 
-eval "$BUILD_CMD"
+eval "$BUILD_PROJECT_CMD"
 
 echo "window.appSettings = {'semver': 'amplify.${AWS_BRANCH}'};" > "./$BUILD_DIR/assets/settings.js"
 echo "{\"amplify\":\"true\",\"commit\":\"${AWS_COMMIT_ID}\",\"semver\":\"${AWS_BRANCH}\"}" > "./$BUILD_DIR/assets/settings.json"
 
-mkdir -p "./asset-output$APP_BASE_PATH"
+mkdir -p "./asset-output/$APP_BASE_PATH"
 cp -R "./$BUILD_DIR/index.html" ./asset-output/
-cp -R "./$BUILD_DIR/." "./asset-output$APP_BASE_PATH"
+cp -R "./$BUILD_DIR/." "./asset-output/$APP_BASE_PATH"
