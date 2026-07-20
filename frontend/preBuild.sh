66c21b21bafee79113dbd5fbc0bc490baea9ac4b
Vasyl Herman <vasyl.herman@underdefense.com>
2026-07-20 11:59:06 +0300

    fix(mx-444): add build sh

--- frontend/preBuild.sh
+++ frontend/preBuild.sh
@@ -7,4 +7,19 @@ NVM_NODE_VERSION="${NVM_NODE_VERSION:-v22}"
 
 nvm install "$NVM_NODE_VERSION"
 nvm use "$NVM_NODE_VERSION"
-envsubst < .npmrc-example > "$HOME/.npmrc"
+
+# Configure npm to pull private @maxi-ui packages from AWS CodeArtifact using a short-lived
+# token minted via the Amplify build role.
+echo "Configuring npm for CodeArtifact (@maxi-ui)..."
+CA_DOMAIN=underdefense
+CA_OWNER=855501706185
+CA_REGION=eu-west-1
+CA_REPO=maxi
+CA_HOST="${CA_DOMAIN}-${CA_OWNER}.d.codeartifact.${CA_REGION}.amazonaws.com"
+CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token \
+  --domain "$CA_DOMAIN" --domain-owner "$CA_OWNER" --region "$CA_REGION" \
+  --query authorizationToken --output text)
+{
+  printf '@maxi-ui:registry=https://%s/npm/%s/\n' "$CA_HOST" "$CA_REPO"
+  printf '//%s/npm/%s/:_authToken=%s\n' "$CA_HOST" "$CA_REPO" "$CODEARTIFACT_AUTH_TOKEN"
+} > "$HOME/.npmrc"
